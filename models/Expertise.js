17f3cc8f403c0924d0cc97f1302f554954d62ed4
Jasmine <105261487+codewithjazzy@users.noreply.github.com>
2024-03-01 19:07:30 -0800

    set up list-like models

---
+++ models/Expertise.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const expertiseSchema = new mongoose.Schema({
+    expertise: { 
+        type: String, 
+        required: true 
+    },
+});
+
+const Expertise = mongoose.model('Expertise', expertiseSchema);
+
+module.exports = Expertise;
