0dd0e39cce1c2c6e45c10a270e10aba507bba0e2
Jasmine <105261487+codewithjazzy@users.noreply.github.com>
2024-06-12 20:12:50 -0700

    added model schemas for DB connection. added dependencies

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,27 @@
 import express from "express";
+const app = express();
 import ViteExpress from "vite-express";
 import "dotenv/config"
 import mongoose from "mongoose";
 import openai from "./config/openaiclient"
+import connectDB from "./config/database";
+import session from "express-session"
+import MongoStore from "connect-mongo";
+import morgan from "morgan";
 
-const app = express();
+
+connectDB();
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+  })
+);
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
---
+++ src/server/models/Profile.js
@@ -0,0 +1,86 @@
+import validator from 'validator';
+import mongoose from "mongoose"; // connects to and communicates with database to use schemas
+
+// Profile Schema
+const profileSchema = new mongoose.Schema({
+  image: {
+    type: String,
+  },
+  cloudinaryId: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+  journey: {
+    type: String,
+  },
+  heritage: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return validator.isEmail(v);
+      },
+      message: props => `${props.value} is not a valid email address!`
+    }
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return validator.isMobilePhone(v, 'en-US');
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
+  website: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return validator.isURL(v);
+      },
+      message: props => `${props.value} is not a valid URL!`
+    }
+  },
+  expertise: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Expertise",
+  }],
+  treatments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Treatment",
+  }],
+  languages: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Language",
+  }],
+  dialects: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Dialect",
+  }],
+  location: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Location",
+  },
+});
+
+const Profile = mongoose.model("Profile", profileSchema);
+
+export default Profile
